c18294364a9fc9ba633c3fb827309823d033d45d
Md-Nur <md.nurealamsiddiquee2004@gmail.com>
2024-08-16 17:06:08 +0600

    setup

---
+++ src/router.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import ErrorPage from "./error-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+      },
+    ],
+  },
+]);
+
+export default router;
