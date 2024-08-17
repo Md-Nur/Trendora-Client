794da56b6caab0b9ee2c58aa5230678f7b26cce1
Md-Nur <md.nurealamsiddiquee2004@gmail.com>
2024-08-17 19:17:38 +0600

    set up necessary ui components

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import axios from "axios";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   axios.defaults.baseURL = import.meta.env.VITE_API_URL!;
   axios.defaults.withCredentials = true;
   return (
     <>
+      <Navbar />
       <Outlet />
+      <Footer />
       <ToastContainer
         position="top-right"
         autoClose={5000}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
+import Hero from "../components/Hero";
+
 const Home = () => {
   return (
     <div>
+      <Hero />
       <h1>Home</h1>
     </div>
   );
